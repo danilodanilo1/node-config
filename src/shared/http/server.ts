eff99a6437b8c9ec542693ec0368628812ba785f
danilo.nascimento <danilo.nascimento@opah.com.br>
2022-02-02 17:51:10 -0300

    competando

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata'
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
@@ -10,19 +11,21 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction)=>{
-  if ( error instanceof AppError){
-    return response.status(error.statusCode).json({
-      status: 'error',
-      message: error.message,
-    })
-  }
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error'
-  })
-})
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! 🛫 ');
